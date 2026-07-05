99d6c7ca16f56dc931f9ed515524ba0c719c102e
ebordenave <edward.bordenave@gmail.com>
2026-07-05 12:01:11 -0400

    complete assignments 1a and 1b

--- assignment1/callsync.js
+++ assignment1/callsync.js
@@ -1,22 +1,22 @@
 function syncfunc() {
-    console.log("In syncfunc.  No async operations here.")
-    return "Returned from syncfunc."
+  console.log("3. In syncfunc.  No async operations here."); // Log: #3
+  return "7. Returned from syncfunc."; // Log: #7
 }
 
 async function asyncCaller() {
-    console.log("About to wait.")
-    const res = await syncfunc()
-    console.log(res)
-    return "asyncCaller complete."
+  console.log("2. About to wait."); // Log: #2
+  const res = await syncfunc();
+  console.log(res);
+  return "9. asyncCaller complete."; // Log: #9
 }
 
-console.log("Calling asyncCaller.")
-const r = asyncCaller()
-console.log(`Got back a value from asyncCaller of type ${typeof r}`)
+console.log("1. Calling asyncCaller."); // Log: #1
+const r = asyncCaller();
+console.log(`4. Got back a value from asyncCaller of type ${typeof r}`); // Log: #4
 if (typeof r == "object") {
-    console.log(`That object is of class ${r.constructor.name}`)
+  console.log(`5. That object is of class ${r.constructor.name}`); // Log: #5
 }
-r.then(resolvesTo => {
-    console.log("The promise resolves to: ", resolvesTo)
-})
-console.log("Finished.")
+r.then((resolvesTo) => {
+  console.log("8. The promise resolves to: ", resolvesTo); // Log: #8
+});
+console.log("6. Finished."); // Log: #6
