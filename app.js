38fe1c956e85bb75bf20ef00e84b16e675e4e47c
ebordenave <edward.bordenave@gmail.com>
2026-09-04 06:29:21 -0400

    render setup

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(
 );
 app.use(helmet());
 
-app.use(express.json());
+app.use(express.json({ limit: "1mb" }));
 app.use(cookieParser());
 app.use(xss());
 
@@ -39,7 +39,7 @@ app.get("/health", async (req, res) => {
   }
 });
 
-app.use("/user", userRouter);
+app.use("/api/users", userRouter);
 app.use("/api/tasks", taskRouter);
 app.use("/api/analytics", analyticsRouter);
 
